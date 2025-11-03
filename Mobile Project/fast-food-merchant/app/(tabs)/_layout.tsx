f75ced7f8457092df442fde28c366efa3453c288
anthonism <134544041+anthonysimonIsCoding@users.noreply.github.com>
2025-11-03 14:03:57 +0700

    Initialize Expo Fast Food Merchant app
    Set up a new Expo project for the Fast Food Merchant app with file-based routing, tab navigation, theming, and starter components. Includes configuration files, assets, hooks, UI components, and removes the old README.

---
+++ Mobile Project/fast-food-merchant/app/(tabs)/_layout.tsx
@@ -0,0 +1,85 @@
+import { HapticTab } from '@/components/haptic-tab';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
+import React from 'react';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarButton: HapticTab,
+        tabBarActiveTintColor: '#D7A359',
+        tabBarInactiveTintColor: '#999',
+
+        // 🔥 Hiệu ứng chuyển tab mượt như iOS
+        animation: 'shift', // hoặc thử 'slide_from_right'
+        tabBarStyle: {
+          backgroundColor: '#fff',
+          borderTopColor: '#eee',
+          height: 70,
+          paddingBottom: 10,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Đơn hàng',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="home"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="notification"
+        options={{
+          title: 'Thông báo',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="location"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="store"
+        options={{
+          title: 'Cửa hàng',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="search"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="promo"
+        options={{
+          title: 'Khuyến mãi',
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons
+              name="person"
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
