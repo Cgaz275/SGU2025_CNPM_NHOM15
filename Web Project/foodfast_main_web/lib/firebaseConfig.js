8f1edf8bdcc91c3fd2817c87db10c976b5387da7
CGA <113306447+Cgaz275@users.noreply.github.com>
2025-11-15 23:07:49 +0700

    Update firebase config

--- Web Project/foodfast_main_web/lib/firebaseConfig.js
+++ Web Project/foodfast_main_web/lib/firebaseConfig.js
@@ -7,14 +7,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Cấu hình web app của bạn
 const firebaseConfig = {
-  apiKey: "AIzaSyDgS3Ux6yWwtY6QvAkagNzW7FWE_d1nILc",
-  authDomain: "foodfast-e293b.firebaseapp.com",
-  databaseURL: "https://foodfast-e293b-default-rtdb.firebaseio.com",
-  projectId: "foodfast-e293b",
-  storageBucket: "foodfast-e293b.firebasestorage.app",
-  messagingSenderId: "1081475813043",
-  appId: "1:1081475813043:web:952fb6260de7142b4f6ac6",
-  measurementId: "G-MLE014NM90"
+  apiKey:  process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain:  process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL:  process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId:  process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket:  process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId:  process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId:  process.env.NEXT_PUBLIC_APP_ID,
+  measurementId:  process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Khởi tạo Firebase
